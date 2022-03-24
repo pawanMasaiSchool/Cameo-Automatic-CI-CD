582c7943a97fd98bf38d65d1a18f4e3ec75638d2
Pawan Sukhwani <sukhwanipawan@gmail.com>
2022-03-24 12:59:44 +0530

    deploying cameo backend on heroku

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const mongoose= require('mongoose')
+require('dotenv').config()
+const uri = process.env.MONGODB_URI;
+const connect= ()=>{
+    return mongoose.connect(uri)
+}
+
+module.exports=connect
